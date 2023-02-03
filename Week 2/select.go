2291ad6b7c6405c0ebc5139ed332f4c1cce328f4
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2023-02-03 20:32:02 +0530

    Week2

--- Week 2/select.go
+++ Week 2/select.go
@@ -6,52 +6,65 @@ import (
 )
 
 // func main() {
-// 	// Sending multiple data on the channel
 // 	channel := make(chan int)
 // 	go func() {
 // 		channel <- 1
-// 		time.Sleep(time.Second * 1)
+// 		time.Sleep(time.Second)
 // 		channel <- 2
-// 		time.Sleep(time.Second * 1)
-// 		channel <- 3
 // 		close(channel)
-// 		// here the main function is waiting for the channel to receive something
-// 		// So, if we are sending multiple messages in a channel, then you need to close the channel, this allows the main function not to wait for any recieving messages
+
 // 	}()
 // 	for ch := range channel {
 // 		fmt.Println(ch)
 // 	}
 // }
 
+func sendInt(ch chan int) {
+	ch <- 1
+}
+func sendBool(ch chan bool) {
+	ch <- true
+}
+func main321() {
+	ch1 := make(chan int)
+	ch2 := make(chan bool)
+	go sendBool(ch2)
+	go sendInt(ch1)
+	select {
+	case getInt := <-ch1:
+		fmt.Println(getInt)
+	case getBool := <-ch2:
+		fmt.Println(getBool)
+		// default:
+		// 	fmt.Println("bye")
+	}
+}
+
 // func main() {
 // 	helloCh := make(chan string, 1)
 // 	goodByeCh := make(chan string, 1)
 // 	quitCh := make(chan bool)
+// 	go receiveMessage(helloCh, goodByeCh, quitCh)
+// 	go sendMessage(helloCh, "Hello world")
+// 	time.Sleep(time.Second)
+// 	go sendMessage(goodByeCh, "Good Bye world")
 
-// 	go ReceiveMsg(helloCh, goodByeCh, quitCh)
-// 	go SendMsg(helloCh, "Hello, world!")
-// 	time.Sleep(time.Second * 1)
-// 	go SendMsg(goodByeCh, "Goodbye, world!")
-// 	res := <-quitCh
-// 	fmt.Println("Message from quitCh:", res)
-// }
-
-func SendMsg(ch chan<- string, message string) {
-	// This is a sender goroutine
+//		fmt.Println("message from quitCh=", <-quitCh)
+//	}
+func sendMessage(ch chan<- string, message string) {
 	ch <- message
 }
-
-func ReceiveMsg(helloCh, goodByeCh <-chan string, quitCh chan<- bool) {
+func receiveMessage(helloCh, goodByeCh <-chan string, quitCh chan<- bool) {
 	for {
 		select {
 		case message := <-helloCh:
-			fmt.Println("Message from helloCh:", message)
+			fmt.Println("Message from helloCh:	", message)
 		case message := <-goodByeCh:
-			fmt.Println("Goodbye from goodByeCh:", message)
+			fmt.Println("Message from goodByeCh:	", message)
 		case <-time.After(time.Second * 2):
-			fmt.Println("Nothing Received in 2seconds, Exiting the receiveMsg goroutine")
+			fmt.Println("Nothing receiving in 2 seconds:	Exiting the receiveMessage goroutine")
 			quitCh <- true
-			return
+			break
 		}
 	}
 }
